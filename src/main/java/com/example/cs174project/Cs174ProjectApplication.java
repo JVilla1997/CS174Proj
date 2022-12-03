b8a6a5176b2a3243b7a2ae9347df81985a69b24e
JVilla1997 <jorge.villavicencio.013@gmail.com>
2022-12-02 21:52:15 -0800

    set up for the backend

---
+++ src/main/java/com/example/cs174project/Cs174ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs174project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs174ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs174ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs174project/Cs174ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs174project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs174ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
